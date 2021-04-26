d059a6ef514a3deebb20deab344bddf050f02aa1
Hong-gyu <hkscprod@naver.com>
2021-04-26 17:35:31 +0900

    자바 1일차 : 변수명 작성 규칙 예제

---
+++ Java_shg/src/day1/Variable2.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		// int num1 = 2; // 에러발생 ; 중복선언
+		// int 1num = 1; // 에러발생 ; 숫자로 시작 할 수 없음
+		int num$ = 1;
+		// int num% = 1; // 에러발생 ; $ 와 _가 아닌 문자 사용
+		int _num$ = 1;
+        // int public = 1; // 에러발생 ; 예약어 사용
+        // 학생수를 저장하기 위한 변수를 생성해야 한다
+		int a = 5; // 변수는 기본적으로 소문자, 변수명은 의미있는 단어로 구성해야함 현재 a 가 무슨 변수인지 모름
+		int studentCount = 5; // 카멜표기법 :두 영단어로 구성된 경우, 두번째 단어부터 첫글자만 대문자로 사용 
+  	}
+
+}
