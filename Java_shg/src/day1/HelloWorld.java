bf0e05fb6825f9acf03409c8bf446783f423ecf6
Hong-gyu <hkscprod@naver.com>
2021-04-26 17:34:28 +0900

    자바 1일차 : 주석 설명 예제

---
+++ Java_shg/src/day1/HelloWorld.java
@@ -0,0 +1,42 @@
+package day1;
+//한줄주석
+/* 여러줄 주석
+ * 여러줄 주석
+ * 여러줄 주석 여러줄 주석
+ */
+/** 도큐먼트 주석 */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// 주석
+		/* 작성일 : 2021-04-26
+		 * 작성자 : 손홍규
+		 * 내용 : 기본 프로젝트 생성 및 테스트 정리할땐 주석을 사용한다
+		 */
+		/** 도큐먼트 주석
+		 */
+		System.out.println("Hello World");
+		
+		System.out.println("자바!");
+		
+		System.out.println(3);
+		
+		System.out.println('a');
+		
+		// sysout + ctrl space
+		// Run : ctrl F11
+System.out.println("자바!");
+	}
+
+	// 홍길동  = 값(상황에 따라 다름)
+	// 이름 홍길동 = 이름 정의(여기서 "이름"은 변수  - 값을 관리하기 위한 도구)  -> [ 변수 - 값 ]
+	// 변수 : 변하는 수
+	// 변수선언 : 변수타입v변수이름; 또는 변수타입v변수이름=값;
+	// 변수타입 : 정수형(소수점이 없는 수-값예시 : -1, 0, 1, 2 ... ) vs 실수형(소수점이 있는 수-값예시 : 3.14, 1.0 ... vs 논리형(참 또는 거짓 true, false)
+	// 정수타입 : byte(1바이트), char(2바이트, 문자관리), short, int, long
+	// 정수형 변수의 바이트가 커질수록 표현할 수 있는 정수 범위가 넓어진다. 주로 정수는 일반적으로 int를 사용하고 큰 정수는 long을 사용
+	// 실수타입 : float, double
+	// 논리타입 : boolean
+
+	
+}
