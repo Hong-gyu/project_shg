a38703b94ae6f18e89e8250be7ac2853436f7ab2
Hong-gyu <hkscprod@naver.com>
2021-04-26 17:37:07 +0900

    자바 1일차 : 연습문제

---
+++ Java_shg/src/day1/Test1_1.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class Test1_1 {
+
+	public static void main(String[] args) {
+		// 여기 부분은 계속 연습을 해야함(어떤 변수명을 쓸 것인지)
+		// 사과의 갯수 5개를 변수에 저장하려 한다. 이때 필요한 변수를 선언하면?
+		int apple = 5;
+		int appleCount = 5;
+		System.out.println(apple);
+		// 수학 국어 영어 성적을 저장하려 한다. 이때 필요한 변수를 선언(단 성적은 0~100점 사이, 소수점은 없음)
+		int mathScore;
+		int korScore;
+		int engScore;
+		
+		// 수학, 국어, 영어 성적의 평균을 저장하려 한다. 이때 필요한 변수 선언
+		double average;
+		
+		// 학생 이름을 저장하기 위한 변수 선언
+		String name;
+		
+		// 과학 성적의 학점을 저장하기 위한 변수 선언(단, 성적은 A,B,C,D,F)
+		char sciScore;
+		
+		// 음악 과목의 이수 여부를 확인하기 위한 변수 선언
+		boolean passMusic; 
+
+		
+	}
+
+}
