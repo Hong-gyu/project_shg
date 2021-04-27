284338f6494c49f88c755da6e73ae027d8eb9a92
Hong-gyu <hkscprod@naver.com>
2021-04-27 17:42:47 +0900

    자바 2일차 : 비교연산자 예제

---
+++ Java_shg/src/day2/CompareOperator.java
@@ -0,0 +1,35 @@
+package day2;
+
+public class CompareOperator {
+
+	public static void main(String[] args) {
+		// 비교연산자 예제
+		
+		System.out.println(" 1 >  2 : " + ( 1 >  2));
+		System.out.println(" 1 >= 2 : " + ( 1 >= 2));
+		System.out.println(" 1 <  2 : " + ( 1 <  2));
+		System.out.println(" 1 <= 2 : " + ( 1 <= 2));
+		System.out.println(" 1 == 2 : " + ( 1 == 2));
+		System.out.println(" 1 != 2 : " + ( 1 != 2));
+		
+		// 4는 짝수인가? 를 확인하기 위한 코드 작성
+		
+		System.out.println("4는 짝수인가? " + (4 % 2 == 0));
+		System.out.println("3은 짝수인가? " + (3 % 2 == 0));
+		
+		System.out.println("0,1과 0.1f가 다른가? " + (0.1 != 0.1f)); // double 로 표현한 0.1과 float으로 표현한 0.1은 다르다(p.120)
+		System.out.println('A' == 65); // 유니코드 표에 문자 A는 숫자 65로 표현되기 때문에 같음
+		
+		
+		
+		
+				
+				
+		
+		/**
+		
+		*/ 
+
+	}
+
+}
