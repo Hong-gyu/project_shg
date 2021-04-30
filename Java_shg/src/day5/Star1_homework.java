bd8c1be2e6336005d6ca1a325bdfe716e538a30e
Hong-gyu <hkscprod@naver.com>
2021-04-30 17:38:09 +0900

    자바 5일차 : Star

---
+++ Java_shg/src/day5/Star1_homework.java
@@ -0,0 +1,93 @@
+package day5;
+
+public class Star1_homework {
+
+	public static void main(String[] args) {
+		/* ****	i = 1	*=4
+		 * ****	i = 2	*=4
+		 * ****	i = 3	*=4
+		 * ****	i = 4	*=4
+		 * ****	i = 5	*=4
+		 */
+		
+		int i, k;
+		for(k=1;k<=5;k+=1) { // 줄이 바뀌면 여기를 바꾸고
+			for(i=1;i<=4;i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/* *		k=1		* = 1
+		 * **		k=2		* = 2
+		 * ***		k=3		* = 3
+		 * ****		k=4		* = 4				
+		 * *****	k=5		* = 5
+		 * 
+		 * */
+		System.out.println();
+		
+		for(k=1;k<=5;k+=1) { // 줄이 바뀌면 여기를 바꾸고
+			for(i=1;i<=k;i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고
+				System.out.print(("*"));
+			}
+			System.out.println();
+		}
+		
+		/*     ****		k=1		* = 1
+		 *     ****		k=2		* = 2
+		 *     ****		k=3		* = 3
+		 *     ****		k=4		* = 4				
+		 *     ****		k=5		* = 5
+		 * 
+		 * */
+		
+		System.out.println();
+		
+		for(k=1;k<=5;k+=1) { // 줄이 바뀌면 여기를 바꾸고
+			for(i=1;i<=4;i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고
+				System.out.print(" ");
+			}
+			for(i=1;i<=4;i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/*       *	k=1 공=4 *=1
+		 *      **	k=2 공=3 *=2
+		 *     ***	k=3 공=2 *=3
+		 *    ****	k=4	공=2 *=4
+		 *   *****	k=5	공=1 *=5
+		 * 
+		 * */
+		
+		
+		System.out.println();
+		
+		for(k=1;k<=5;k+=1) { // 줄이 바뀌면 여기를 바꾸고 (출력 줄 수)
+			for(i=1;i<=(5-k);i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고 (콘솔 출력값 AB 에서 A)
+				System.out.print((" "));
+			}
+			for(i=1;i<=k;i+=1) {  // 별의 갯수가 달라지면 여기를 바꾸고 (콘솔 출력값 AB 에서 B)
+				System.out.print(("*"));
+			}
+			System.out.println();
+		}
+		
+		// 연습문제 
+		/*     *
+		 *    ***
+		 *   *****
+		 *  *******
+		 * *********
+		 * */
+		
+		/* *********
+		 *  *******
+		 *   *****
+		 *    ***
+		 *     *
+		 */
+	}
+}
