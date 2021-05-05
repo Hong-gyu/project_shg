60a0d7ca28aa24f8f9546743a3e352122fd8df63
Hong-gyu <83192202+Hong-gyu@users.noreply.github.com>
2021-05-06 07:00:28 +0900

    자바 7일차 : 로또번호 연습

--- Java_shg/src/day7/Lotto.java
+++ Java_shg/src/day7/Lotto.java
@@ -5,144 +5,98 @@ import java.util.Scanner;
 public class Lotto {
 
 	public static void main(String[] args) {
-		/* 사용자 번호를 랜덤으로 생성하고, 당첨 번호를 입력하면 당첨
-		 * 등수를 출력하는 코드를 작성하세요
-		 * - 사용자 번호 : 1 ~ 45 사이의 수, 중복되지 않음, 6개
-		 * - 당첨번호 : 1 ~ 45 사이의 수, 중복되지 않음, 6개 + 1개(보너스 번호)
-		 *  1등 : 당첨번호 6개 일치
-		 *  2등 : 당첨번호 5개와 보너스 번호 일치
-		 *  3등 : 당첨번호 5개 일치
-		 *  4등 : 당첨번호 4개 일치
-		 *  5등 : 당첨번호 3개 일치
-		 *  꽝 : 당첨번호 2개 이하
+		/* 사용자 번호를 랜덤으로 생성하고, 당첨 번호를 입력 하면 당첨
+		 * 1등 : 당첨번호 6개 일치
+		 * 2등 : 당첨번호 6개 일치 + 1
+		 * 3등 : 당첨번호 5개 일치
+		 * 4등 : 당첨번호 4개 일치
+		 * 5등 : 당첨번호 3개 일치
+		 * 꽝  : 당첨번호 2개 이하
 		 * */
-
-	}
 	
-	/*  컴퓨터가 랜덤한 숫자 6개+1개를 생성
-		int []com6=new int[6];
-		int []com1=new int[1];
-		// 랜덤 범위 지정
-		int min=1, max=45;
-		// 메소드를 이용하여 배열에 랜덤으로 6개 저장(중복x)
-		init(com, min, max);
-		for(int tmp : com) {
-			// System.out.print(tmp +" ");
-		}
-		// 랜덤으로 생성된 컴퓨터 숫자 확인
-		
-		// 사용자 번호 6개 랜덤으로 생성
+		int lotto[]=new int[6];
 		int user[]=new int[6];
-		System.out.println("");
 		
-		do {
-			for(int i=0; i<user.length;i+=1) {
-				user[i]=scan.nextInt();
+		for(int i=0; i<6; i+=1) {
+			lotto[i]=(int)(Math.random()*45);	
+			for(int j=0; j<i;j+=1) {
+				if(lotto[j]==lotto[i]) {
+					j+=1;
+					break;
+				}
 			}
-			// 스트라이크와 볼의 갯수를 셈
-			// 메소드를 이용하여 스트라이크 갯수 셈
-			// 메소드를 이용하여 볼 갯수 셈
-			strike=getStrike(com, user);
-			ball=getBall(com, user);
-			// 메소드를 이용하여 판정
-			printResult(ball, strike);
-		 } while(strike<3);
-		scan.close();
+		}
+		System.out.print("로또번호 : ");
+		
+		for(int i=0;i<6;i+=1) {
+			System.out.print(lotto[i]+" ");
+		}
+
+		System.out.println("");
+		System.out.print("번호를 입력하세요 : ");
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
 	}
 	
-	/* 기능 : 배열 arr에 1번지부터 count개 까지 확인하여 정수 num가 있는지 없는지 확인하여 알려주는 메소드
-	 * 매개변수 : 배열 arr, 정수 num => int [] arr, int count, int num
-	 * 리턴타입 : 있는지 없는지 => 참 거짓 boolean
-	 * 메소드명 : contains
-	 * 설명 : [1,2,3,4,5]배열에서 처음부터 3개 중에서 5가 있는지 없는지 확인 -> 없음 
-	 * 		[1,2,3,4,5]배열에서 처음부터 5개 중에서 5가 있는지 없는지 확인 -> 있음
+	/* 기능 : 배열이 주어지고, 주어진 배열에서 0번지 부터 count개 까지 확인하여 정수 num가 있는지 없는지 확인하는 메소드(배열에 정수 num 가 있는지 중복체크 )
+	 * 매개변수 : 배열([1,2,3,4,5]), 정수 num(7) => int [] arr, int num
+	 *			갯수 => int count
+	 * 리턴타입 : 있는지 없는지 => 참 또는 거짓 boolean
+	 * 메소드명 : contains 
 	 * */
-	
-	public static boolean contains(int [] arr, int count, int num) {
-		for(int i=0; i < count; i+=1) {
-			if(arr[i]==num) {
+
+	public static boolean contains(int []arr, int num, int count) {
+		if(arr.length<count) { // 예외처리 : 배열의 크기보다 count가 큰 경우
+			count=arr.length; // 비교할 갯수를 배열의 크기로 수정
+		}
+		for(int i=0;i<count;i+=1) {
+			if(arr[i]==num) { // 배열의 값과 num의 값이 같은 경우(중복된 경우
 				return true;
 			}
 		}
-		return false;
+		return false;			
 	}
 	
-	/* 기능 : 배열 arr가 주어지면 주어진 배열에 min 에서 max 한 숫자를 생성하여 중복되지 않게 저장하는 메소드
-	 * 매개변수 : 배열 arr, 랜덤 범위 정수 min, 정수 max => int [] arr, int min, int max
-	 * 리턴타입 : 없음 => void
-	 * 메소드명 : init
-	 * 설명 :  
-	 * 		
-	 * */	
-	
-	public static void init(int [] arr, int min, int max) {
-		int cnt = 0; // 저장된 숫자의 갯수
-		while(cnt < arr.length) {
-			int r=(int)(Math.random()*(max-min+1)+1);
-			if(!contains(arr, cnt, r)) {
-				arr[cnt]=r;
-				cnt+=1;
-			}
-		}
-	}
-	
-	/* 기능 : 배열 arr1과 배열 arr2에서 같은 위치의 숫자를 비교하여 같은 숫자의 갯수를 알려주는 메소드
+	/* 기능 : 컴퓨터가 만든 배열과 사용자가 입력한 배열에서 같은 위치의 숫자를 비교하여 같은 숫자의 갯수를 알려주는 메소드
 	 * 매개변수 : 배열 arr1, 배열 arr2 => int [] arr1, int [] arr2
 	 * 리턴타입 : 같은 숫자의 갯수 => 정수 => int 
-	 * 메소드명 : getStrike
+	 * 메소드명 : 같은숫자
 	 * 설명 : [1, 2, 3], [3, 4, 5]가 있을때 0 strike
 	 * 		 [1, 2, 3], [1, 9, 4]가 있을때 1 strike 
 	 * */
 	
-	public static int getStrike(int []arr1, int []arr2) {
+	public static int 같은숫자 (int lotto[], int user[]) {
 		int cnt = 0;
-		for(int i=0; i<arr1.length;i+=1) {
-			if(arr1[i]==arr2[i]) {
+		for(int i=0; i<lotto.length;i+=1) {
+			if(lotto[i]==user[i]) {
 				cnt+=1;
 			}
 		}
 		return cnt;
 	}
 	
-	/* 기능 : 배열 arr1과 배열 arr2에서 같은 자리가 아닌 수들 중에서 같은 숫자의 갯수를 알려주는 메소드 
-	 * 매개변수 : 배열 arr1, 배열 arr2 => int [] arr1, int [] arr2
-	 * 리턴타입 : 같은 숫자리가 아닌 수들 중에서 같은 숫자의 갯수 => 정수 => int 
-	 * 메소드명 : getBall
-	 * 설명 : [1, 2, 3], [3, 2, 4]가 있을때 1 ball -> 같은 숫자의 갯수(2) - 스트라이크 갯수(1) = 볼의 갯수(1)
-	 * 		 [1, 2, 3], [1, 2, 4]가 있을때 0 ball 
-	 * */
-	
-	public static int getBall(int []arr1, int []arr2) {
-		int cnt = 0; // 같은 숫자의 갯수
-		for(int i=0; i<arr1.length;i+=1) {
-			if(contains(arr2, arr2.length,arr1[i])){
-				cnt+=1;
-			}
-		}
-		return cnt-getStrike(arr1, arr2);
-	}
-	
-	/* 기능 : ball 과 strike 의 수에 따라 결과를 출력하는 메소드 
-	 * 매개변수 : ball, strike => int ball, int strike
+	/* 기능 : 같은 수에 따라 결과를 출력하는 메소드 
+	 * 매개변수 : 같은수 => int ball, int strike
 	 * 리턴타입 : 출력은 없음 => void 
 	 * 메소드명 : printResult
 	 * */
 	
 	public static void printResult(int ball, int strike) {
-		if(strike!=0) {
-			System.out.print(strike+"S");
+		if(컴퓨터 배열=사용자 입력 배열 6개) {
+			System.out.print("1등");
 		}
-		if(ball!=0) {
-			System.out.print(ball+"B");
+		if(컴퓨터 배열=사용자 입력 배열 5개) {
+			System.out.print("2등");
 		}
-		if(strike==0 && ball==0) {
-			System.out.print("O");
+		if(컴퓨터 배열=사용자 입력 배열 4개) {
+			System.out.print("3등");
+		}
+		if(컴퓨터 배열=사용자 입력 배열 3개) {
+			System.out.print("4등");
+		}
+		if(컴퓨터 배열=사용자 입력 배열 2개) {
+			System.out.print("5등");
 		}
 		System.out.println();
 	}
-}
-
-	 * 
-	 * */
-
 }
