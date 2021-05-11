a2534e028d5405a9b0980acc0ea5430c7e93131b
Hong-gyu <hkscprod@naver.com>
2021-05-11 17:45:09 +0900

    자바 10일차 : Class 예제6

--- Java_shg/src/day10/ClassEx6.java
+++ Java_shg/src/day10/ClassEx6.java
@@ -1,6 +1,6 @@
 package day10;
 
-public class ClassEx_6 {
+public class ClassEx6 {
 
 	public static void main(String[] args) {
 		
@@ -15,6 +15,9 @@ public class ClassEx_6 {
 		Rect r3 = new Rect(lu,rd);
 		r3.print();
 		lu.setX(-10); // 점을 수정했더니 사각형의 점의 위치가 같이 수정되는 문제 발생
+		// 복사생성자를 이용해서 해결(point에 저장되어 있는 생성자에 추가로 lu와 rd를 받을 수 있는 생성자를 추가하여 점의 위치를 고정시킴
+		r3.print();
+		r3.move(-20, 0);
 		r3.print();
 
 	}
