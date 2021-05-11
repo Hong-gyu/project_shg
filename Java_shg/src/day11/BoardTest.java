08130b864767618ca3ef6061857799c49f531ff9
Hong-gyu <hkscprod@naver.com>
2021-05-11 17:43:02 +0900

    자바 11일차 : 게시글 예제

---
+++ Java_shg/src/day11/BoardTest.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class BoardTest {
+
+	public static void main(String[] args) {
+		Board board = new Board(1, "안녕하세요", "반갑습니다", "홍길동", "2021-05-11, 12:00", "공지사항");
+		board.print();
+	}
+
+}
