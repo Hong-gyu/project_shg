872f841757100dab7af6426ebfa7f190e256cb3b
Hong-gyu <hkscprod@naver.com>
2021-05-13 17:46:02 +0900

    자바 12일차 : 자동수동자동차

---
+++ Java_shg/src/day12/AutoCar.java
@@ -0,0 +1,24 @@
+package day12;
+
+import day13.Car;
+
+public class AutoCar extends Car {
+	
+	public AutoCar(String type, String company) {
+		super(type, company);
+		super.setGear('P');
+	}
+	@Override
+	public void setGear(char gear) {
+		switch(gear) {
+		case 'D', 'N', 'R', 'P' : 
+			super.setGear(gear);
+			break;
+		default : 
+			super.setGear('P');
+		}
+	}
+	
+	
+
+}
