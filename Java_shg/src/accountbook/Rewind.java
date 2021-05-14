9992399f6036e2dd5045ed5de75da9a6813459d4
Hong-gyu <hkscprod@naver.com>
2021-05-14 17:42:12 +0900

    자바 14일차 : 복습

---
+++ Java_shg/src/accountbook/Rewind.java
@@ -0,0 +1,11 @@
+package accountbook;
+
+public class Rewind {
+
+	public static void main(String[] args) {
+		/* 1. 클래스 고민(클래스를 만드는 연습이 우선 -> 클래스 만드는 것이 익숙해져야 함)
+		 *  예 : 수강생 관리 프로그램을 만드려고 한다.(상황제시) 이때 필요한 클래스는 무엇이 있는가?(연습)*/
+
+	}
+
+}
