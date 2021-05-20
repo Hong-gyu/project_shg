c2c4ae9a6e4f098eefdcfc859715b28bb56bf94c
Hong-gyu <hkscprod@naver.com>
2021-05-20 17:44:05 +0900

    자바 17일차 : 스캐너 예제

---
+++ Java_shg/src/day17/ScannerTest.java
@@ -0,0 +1,38 @@
+package day17;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		/* next() : 공백(엔터, 스페이스)을 만나기전까지 문자열을 가져오는 메소드
+		 * nextLine() : 처음 만나는 엔터 전까지 문자열을 가져오는 메소드 
+		 * */
+		Scanner scan=new Scanner("4321 1234\n\n\n4567\n8910\n+++++\n");
+		while(scan.hasNext()) {
+			String str = scan.nextLine();
+			if(str.length() != 0) {
+			System.out.println(str);
+			}
+		}
+		// String str = scan.next();
+		/* String str = scan.nextLine();
+		System.out.println(str); // 4321 1234 추출
+		System.out.println("---------");
+		str = scan.nextLine();	// 4567 추출
+		System.out.println(str);
+		System.out.println("---------");
+		int num = scan.nextInt();	// 8910 추출
+		System.out.println(num);
+		System.out.println("---------");
+		str = scan.nextLine();	// 빈 공백 추출
+		System.out.println(str);
+		System.out.println("---------");
+		str = scan.next();	// +++++ 추출
+		System.out.println(str);
+		System.out.println("---------"); */
+				
+		scan.close();
+	}
+
+}
