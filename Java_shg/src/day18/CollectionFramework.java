d66f2d2636c9077348a18e0fce3bbfbe9e599342
Hong-gyu <hkscprod@naver.com>
2021-05-21 17:44:00 +0900

    자바 18일차 : 컬렉션 프레임 워크

---
+++ Java_shg/src/day18/CollectionFramework.java
@@ -0,0 +1,46 @@
+package day18;
+
+public class CollectionFramework {
+
+	public static void main(String[] args) {
+		/* 컬렉션 프레임워크 : 자료들을 효율적으로 관리하기 위한 것 
+		 * 종류 : List, Set, Map
+		 * 	List와 Set은 한 종류의 데이터 관리하고 Map은 두 종류의 데이터 관리
+		 * 	List : 순서 보장, 중복 가능
+		 * 	Set : 중복 불가능, 순서 보장 안함
+		 * 	Map : key 와 value 로 이루어짐.
+		 * 		key : 중복 불가(ID)
+		 * 		value : 중복 가능(PW)
+		 * */
+		
+		/* List 특징 
+		 * 	- 순서보장, 중복가능
+		 *  - 인터페이스
+		 * List 종류  
+		 * 	- ArrayList : List의 구현클래스
+		 *  	- 특징 : 배열로 된 리스트
+		 *  	- 장점 : 탐색이 빠름
+		 *  	- 단점 : 중간 삽입/삭제가 느림
+		 * 	- LinkedList : List의 구현클래스
+		 * 		- 특징 : 연결로 된 리스트
+		 * 		- 장점 : 중간 삽입/삭제가 빠름 
+		 * 		- 단점 : 탐색이 느림 
+		 * */
+		
+		/* Set 특징 
+		 * 	- 중복 불가능, 순서 보장 안함
+		 * 	- 인터페이스
+		 * Set 종류
+		 * 	- HashSet : Hash코드를 이용하여 먼저 확인 후 equals()를 이용하여 확인	*/
+		
+		/* Map 특징 
+		 * 	- key와 value로 이루어짐
+		 * 	- key는 중복 불가, value는 중복 가능
+		 *  - 출력할 때 조금 복잡
+		 *  - HashMap
+		 *  	=> Map 구현 클래스
+		 *  	=> 	*/
+
+	}
+
+}
