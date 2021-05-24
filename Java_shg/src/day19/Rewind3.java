2ce437ece4be184ed9b3e4ea6951b21ba4c0ad10
Hong-gyu <hkscprod@naver.com>
2021-05-24 17:44:54 +0900

    자바 19일차 : 조건문

---
+++ Java_shg/src/day19/Rewind3.java
@@ -0,0 +1,35 @@
+package day19;
+
+public class Rewind3 {
+
+	public static void main(String[] args) {
+		/* 조건문
+		 *  - ~(조건식) 하면 ...(실행문) 해라 
+		 *  - 조건문의 종류 : if문, switch문, 
+		 *  	1. if 문 : 모든 조건문을 다룰 수 있음
+		 *		-> if(조건식1) {
+		 *		 		실행문1;
+		 *    	  } else if (조건식2) {
+		 *    			실행문2;
+		 *    	  } else {
+		 *    			실행문3;
+		 *    	  }
+		 *  	-> if : 무조건 1개, else if : 0개 이상, else : 0개 또는 1개
+		 *  	-> 위의 문법에서 조건식1과 조건식2가 동시에 만족하는 경우, 실행되는 실행문은? 실행문1
+		 *  
+		 *  	2. switch 문 : 사용하는 변수(식)의 값이 제한적일 때
+		 *    	-> break : switch문을 빠져나오는 역할
+		 *      -> switch문에서 break를 만나지 못하면 다음 실행문으로 이동 
+		 *  		switch(변수/식) {
+		 *    		case 값 :
+		 *    				실행문
+		 *    				break;
+		 *    		default : 
+		 *    				실행문
+		 *    		}
+		 *    
+		 *  */
+
+	}
+
+}
