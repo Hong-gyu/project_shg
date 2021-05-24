ac70243e03dbeb1a75b9c6c1187d7c8ba729e712
Hong-gyu <hkscprod@naver.com>
2021-05-24 17:44:42 +0900

    자바 19일차 : 기본 API 클래스

---
+++ Java_shg/src/day19/Rewind10.java
@@ -0,0 +1,31 @@
+package day19;
+
+public class Rewind10 {
+
+	public static void main(String[] args) {
+		/* 기본 API 클래스 
+		 * Object : 모든 클래스의 조상클래스
+		 *  - equals(), toString(), ... 
+		 * String 
+		 *  - 문자열이 있는지 확인하는 메소드 : indexOf(), contains()
+		 *  - 문자열이 같은지 확인하는 메소드 : equals()
+		 *  - 구분자를 기준으로 문자열을 추출하여 배열로 만드는 메소드 : split()
+		 *  - 문자열의 길이를 확인하는 메소드 : length()
+		 * Math
+		 *  - 올림 : ceil()
+		 *  - 버림 : floor()
+		 *  - 반올림 : round(), rint()
+		 *  - 랜덤 : random() 
+		 * Date
+		 *  - 날짜를 문자열로 
+		 *  - 문자열을 날짜로 
+		 * Calendar
+		 *  - 달력
+		 *  - 년, 월, 일, 시, 분, 초 등 다양한 값들을 확인 할 수 있음
+		 *  - 추상클래스 */
+		
+		
+
+	}
+
+}
